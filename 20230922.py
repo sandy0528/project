48308e2327425f0c88a70a2981dc150f70bffb69
sandy0528 <112428932+sandy0528@users.noreply.github.com>
2023-09-22 13:52:08 -0600

    Add files via upload

---
+++ 20230922.py
@@ -0,0 +1,15 @@
+print("welcome Python")
+
+fav_mum=3
+out_str='my favourite number'
+print (fav_mum,out_str)
+
+# Python3 program to add two numbers
+num1 = 15
+num2 = 12
+ 
+# Adding two nos
+sum = num1 + num2
+ 
+# printing values
+print("Sum of", num1, "and", num2 , "is", sum)
